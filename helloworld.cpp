f3e9b281fa014591095598c7b9f7cdb0e6ce89a6
vspolyakov <74113563+vspolyakov@users.noreply.github.com>
2021-05-30 21:28:31 +0300

    First simple C++ program.
    Create GitHub repository for C++ samples.

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+/* First simple C++ program.
+ * Ubuntu / Visual Studio Code
+ * Vitaliy Polyakov (C) <polyakov.vs@mail.ru>
+ * Created 2021-05-30.
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
